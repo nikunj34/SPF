f2f5df5dd675ac296f451d09c8c5a629b6cfaa47
nikunj34 <48507843+nikunj34@users.noreply.github.com>
2019-07-07 16:52:36 +0530

    Add files via upload

--- Generator.java
+++ Generator.java
@@ -288,7 +288,7 @@ public class Generator {
 			        String VerbType = VerbList.get(k).VerbType;
 			        
 			        if(VarType=="category")
-			        	seed_file.VerbGenerator(VerbName, VerbType, ColName, pred_file.pred_dict.get(ColName).get(1), pred_file.pred_dict.get(ColName).get(2));
+			        	seed_file.VerbGenerator(VerbName, VerbType, ColName, pred_file.pred_dict.get(ColName).get(2), pred_file.pred_dict.get(ColName).get(3));
 			        
 			        else
 			        	seed_file.VerbGenerator(VerbName, VerbType, ColName, pred_file.pred_dict.get(ColName).get(0), pred_file.pred_dict.get(ColName).get(1));
